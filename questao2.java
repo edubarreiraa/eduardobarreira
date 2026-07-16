84ce43103a4899feef9fcc42e3dc25dc98288a1c
barreirakkjk <eduardossb.xam@gmail.com>
2026-07-16 17:18:42 -0300

    finalizado

--- questao2.java
+++ questao2.java
@@ -9,7 +9,7 @@ public class questao2 {
 
     do {
 
-        System.out.print("Digite um número positivo (negativo para sair): ")
+        System.out.print("Digite um número positivo (negativo para sair): ");
         numero = scanner.nextInt();
 
         if (numero >= 0) {
